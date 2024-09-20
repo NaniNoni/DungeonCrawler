5fdc5f70a21be8a04c0a9002a9b28d5385be5c18
NaniNoni <stoneater03@gmail.com>
2024-09-20 11:01:27 -0700

    Simplify texture loading and load walk animation

--- core/src/main/java/com/github/naninoni/dungeon_crawler/Player.java
+++ core/src/main/java/com/github/naninoni/dungeon_crawler/Player.java
@@ -1,50 +1,37 @@
 package com.github.naninoni.dungeon_crawler;
 
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import java.util.Arrays;
 import java.util.EnumMap;
 
 public class Player {
     public enum PlayerAnimation {
-        Idle
+        Idle,
+        Walk
     }
 
     private PlayerAnimation animationState = PlayerAnimation.Idle;
-    private final EnumMap<PlayerAnimation, Texture> textures = new EnumMap<>(PlayerAnimation.class);
     private final EnumMap<PlayerAnimation, Animation<TextureRegion>> animations = new EnumMap<>(PlayerAnimation.class);
+    Texture playerSheet = new Texture(Gdx.files.internal("Soldier/Soldier/Soldier.png"));
 
     public Player() {
-        textures.put(PlayerAnimation.Idle, new Texture(Gdx.files.internal("Soldier/Soldier/Soldier-idle.png")));
+        final int TEXTURES_PER_ROW = 9;
+        final int TEXTURES_PER_COLUMN = 7;
+        final float FRAME_DURATION = 0.25f;
 
-        // Place the regions into a 1D array in the correct order, starting from the top
-        // left, going across first. The Animation constructor requires a 1D array.
-        int FRAME_COLS = 6;
-        int FRAME_ROWS = 1;
-        TextureRegion[] frames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        TextureRegion[][] regions = TextureRegion.split(playerSheet,
+            playerSheet.getWidth() / TEXTURES_PER_ROW,
+            playerSheet.getHeight() / TEXTURES_PER_COLUMN
+        );
 
-        // TODO: Optimize this bullshit
-        // Use the split utility method to create a 2D array of TextureRegions. This is
-        // possible because this sprite sheet contains frames of equal size and they are
-        // all aligned.
-        for (PlayerAnimation animation : PlayerAnimation.values()) {
-            TextureRegion[][] tmp = TextureRegion.split(textures.get(animation),
-                textures.get(animation).getWidth() / FRAME_COLS,
-                textures.get(animation).getHeight() / FRAME_ROWS
-            );
-
-            int index = 0;
-            for (int i = 0; i < FRAME_ROWS; i++) {
-                for (int j = 0; j < FRAME_COLS; j++) {
-                    frames[index++] = tmp[i][j];
-                }
-            }
-
-            animations.put(animation, new Animation<>(0.025f, frames));
-        }
+        TextureRegion[] idleFrames = Arrays.copyOfRange(regions[0], 0, 6);
+        TextureRegion[] walkFrames = Arrays.copyOfRange(regions[1], 0, 8);
+        animations.put(PlayerAnimation.Idle, new Animation<>(FRAME_DURATION, idleFrames));
+        animations.put(PlayerAnimation.Walk, new Animation<>(FRAME_DURATION, walkFrames));
     }
 
     public TextureRegion getCurrentFrame(float stateTime) {
@@ -59,8 +46,6 @@ public class Player {
     }
 
     public void dispose() {
-        for (Texture texture : textures.values()) {
-            texture.dispose();
-        }
+        playerSheet.dispose();
     }
 }
