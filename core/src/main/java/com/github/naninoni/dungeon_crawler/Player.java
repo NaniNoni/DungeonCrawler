a0e145eb024c8c2a3cceea08fe74c29cfa69e06f
Worlds-law <liam.jhcho@gmail.com>
2024-11-02 21:57:37 +0100

    rollback

--- core/src/main/java/com/github/naninoni/dungeon_crawler/Player.java
+++ core/src/main/java/com/github/naninoni/dungeon_crawler/Player.java
@@ -5,10 +5,11 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.Arrays;
-import java.util.Vector;
 
 public class Player extends AnimatedGameObject<Player.PlayerAnimation> {
     private static Player instance;
@@ -19,6 +20,7 @@ public class Player extends AnimatedGameObject<Player.PlayerAnimation> {
         }
         return instance;
     }
+
     public enum PlayerAnimation {
         IdleFront,
         IdleBack,
@@ -28,26 +30,15 @@ public class Player extends AnimatedGameObject<Player.PlayerAnimation> {
         WalkFront,
         WalkBack,
         WalkLeft,
-        WalkRight,
-        AttackFront,
-        AttackLeft,
-        AttackRight,
-        AttackBack
+        WalkRight
     }
 
     private boolean isMoving = false;
-    private int health;
-    private int maxHealth;
-    private float speed = 400f;
+    private final float speed = 400f;
     private final Texture spriteSheet = new Texture(Gdx.files.internal("sprites/characters/player.png"));
-    private float timeSinceLastHeal = 0f;
-    private final float HEAL_INTERVAL = 3f;  // Heal every 3 seconds
-    private final int HEAL_AMOUNT = 5;
 
     public Player() {
         super(new Vector2(), 50f, PlayerAnimation.IdleFront);
-        maxHealth = 100;
-        health = maxHealth;
 
         final int TEXTURES_PER_ROW = 6;
         final int TEXTURES_PER_COLUMN = 10;
@@ -65,14 +56,13 @@ public class Player extends AnimatedGameObject<Player.PlayerAnimation> {
         TextureRegion[] walkRight = Arrays.copyOfRange(regions[4], 0, 6);
         TextureRegion[] walkBack = Arrays.copyOfRange(regions[5], 0, 6);
         TextureRegion[] attackFront = Arrays.copyOfRange(regions[6], 0, 4);
-        TextureRegion[] attackRight = Arrays.copyOfRange(regions[7], 0, 4);
+        TextureRegion[] attackSide = Arrays.copyOfRange(regions[7], 0, 4);
         TextureRegion[] attackBack = Arrays.copyOfRange(regions[8], 0, 4);
         TextureRegion[] die = Arrays.copyOfRange(regions[9], 0, 3);
 
         // Flipped animations
         TextureRegion[] idleLeft = flipTextureRegions(idleRight);
         TextureRegion[] walkLeft = flipTextureRegions(walkRight);
-        TextureRegion[] attackLeft = flipTextureRegions(attackRight);
 
         animations.put(PlayerAnimation.IdleFront, new Animation<>(FRAME_DURATION, idleFront));
         animations.put(PlayerAnimation.IdleBack, new Animation<>(FRAME_DURATION, idleBack));
@@ -83,76 +73,35 @@ public class Player extends AnimatedGameObject<Player.PlayerAnimation> {
         animations.put(PlayerAnimation.WalkBack, new Animation<>(FRAME_DURATION, walkBack));
         animations.put(PlayerAnimation.WalkLeft, new Animation<>(FRAME_DURATION, walkLeft));
         animations.put(PlayerAnimation.WalkRight, new Animation<>(FRAME_DURATION, walkRight));
-
-        animations.put(PlayerAnimation.AttackFront, new Animation<>(FRAME_DURATION, attackFront));
-        animations.put(PlayerAnimation.AttackBack, new Animation<>(FRAME_DURATION, attackBack));
-        animations.put(PlayerAnimation.AttackLeft, new Animation<>(FRAME_DURATION, attackLeft));
-        animations.put(PlayerAnimation.AttackRight, new Animation<>(FRAME_DURATION, attackRight));
-    }
-    public int getHealth() {
-        return health;
     }
 
-    public void setHealth(int health) {
-        this.health = Math.min(health, maxHealth);
-    }
-
-    public void takeDamage(int damage) {
-        health -= damage;
-        if (health <= 0) {
-            health = 0;
-            die();
-        }
-    }
-
-    public void heal(int healAmount) {
-        health = Math.min(health + healAmount, maxHealth);
-    }
-    public void update(float deltaTime) {
-
-        timeSinceLastHeal += deltaTime;
-
-        if (timeSinceLastHeal >= HEAL_INTERVAL) {
-            heal(HEAL_AMOUNT);
-
-            timeSinceLastHeal = 0f;
-        }
-    }
-
-    private void die() {
-
-
-
-    }
-
-    public void input() {
-        Vector2 direction = new Vector2();
-        update(Gdx.graphics.getDeltaTime());
+    public void input(Viewport viewport) {
+        Vector2 translation = new Vector2();
 
         // Check for WASD key presses and update direction accordingly
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            direction.y += 1;  // Move up
+            translation.y += 1;  // Move up
             setAnimationState(Player.PlayerAnimation.WalkBack);
             setMoving(true);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            direction.y -= 1;  // Move down
+            translation.y -= 1;  // Move down
             setAnimationState(Player.PlayerAnimation.WalkFront);
             setMoving(true);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            direction.x -= 1;  // Move left
+            translation.x -= 1;  // Move left
             setAnimationState(Player.PlayerAnimation.WalkLeft);
             setMoving(true);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            direction.x += 1;  // Move right
+            translation.x += 1;  // Move right
             setAnimationState(Player.PlayerAnimation.WalkRight);
             setMoving(true);
         }
 
         // Not moving
-        if (direction.equals(Vector2.Zero)) {
+        if (translation.isZero()) {
             setMoving(false);
         }
 
@@ -174,27 +123,28 @@ public class Player extends AnimatedGameObject<Player.PlayerAnimation> {
             }
         }
 
-        // Normalize translation vector so that the player doesn't move faster diagonally
-        direction.nor();
+        translation
+            .nor() // Normalize translation vector so that the player doesn't move faster diagonally
+            .scl(getSpeed())
+            .scl(Gdx.graphics.getDeltaTime());
 
-        // Move player based on direction and speed
-        Vector2 translation = direction.scl(getSpeed() * Gdx.graphics.getDeltaTime());
         position.add(translation);
+
+        // Have the camera follow the player
+        viewport.getCamera().position.set(position.x, position.y, 0);
+        viewport.getCamera().update();
     }
 
     public PlayerAnimation getAnimationState() {
         return animationState;
     }
+
     public void setAnimationState(PlayerAnimation animationState) {
         this.animationState = animationState;
     }
 
     public float getSpeed() {
         return speed;
-    }
-
-    public void setSpeed(float speed) {
-        this.speed = speed;
     }
 
     public boolean isMoving() {
@@ -204,6 +154,17 @@ public class Player extends AnimatedGameObject<Player.PlayerAnimation> {
     public void setMoving(boolean moving) {
         isMoving = moving;
     }
+
+    /**
+     * @return The position of the chunk the player is currently in.
+     */
+    public Vector2i getChunk() {
+        return new Vector2i(
+            MathUtils.floor(position.x / (Chunk.CHUNK_SIZE * Chunk.TILE_SIZE)),
+            MathUtils.floor(position.y / (Chunk.CHUNK_SIZE * Chunk.TILE_SIZE))
+        );
+    }
+
     public void dispose() {
         spriteSheet.dispose();
     }
